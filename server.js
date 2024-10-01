33cc899647fb3489be11e815a6d15cebbeb8ed54
Seyam <omarseyam1729@gmail.com>
2024-10-01 18:33:35 +1000

    added new server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const pageRoutes = require('./routes/pageRoutes');
 const authRoutes = require('./routes/authRoutes');
 const problemRoutes = require('./routes/problemRoutes');
 const discussionRoutes=require('./routes/discussionRoutes');
-const PORT=3000;
+const PORT=443;
 const sequelize = require('./config/connection');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
